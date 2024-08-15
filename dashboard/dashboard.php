237e3e9c900c114112c491f47966f7e30c0d4f99
Alez1385 <santiagocaponf@gmail.com>
2024-08-15 17:29:45 +0200

    cambios en el dashboard

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1,3 +1,8 @@
+<?php
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -96,82 +101,34 @@
         <main>
             <h1>Analytics</h1>
             <!-- Analyses -->
-            <div class="analyse">
-                <div class="sales">
-                    <div class="status">
-                        <div class="info">
-                            <h3>Total Sales</h3>
-                            <h1>$65,024</h1>
-                        </div>
-                        <div class="progresss">
-                            <svg>
-                                <circle cx="38" cy="38" r="36"></circle>
-                            </svg>
-                            <div class="percentage">
-                                <p>+81%</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="visits">
-                    <div class="status">
-                        <div class="info">
-                            <h3>Site Visit</h3>
-                            <h1>24,981</h1>
-                        </div>
-                        <div class="progresss">
-                            <svg>
-                                <circle cx="38" cy="38" r="36"></circle>
-                            </svg>
-                            <div class="percentage">
-                                <p>-48%</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="searches">
-                    <div class="status">
-                        <div class="info">
-                            <h3>Searches</h3>
-                            <h1>14,147</h1>
-                        </div>
-                        <div class="progresss">
-                            <svg>
-                                <circle cx="38" cy="38" r="36"></circle>
-                            </svg>
-                            <div class="percentage">
-                                <p>+21%</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
             <!-- End of Analyses -->
 
             <!-- New Users Section -->
             <div class="new-users">
                 <h2>New Users</h2>
                 <div class="user-list">
-                    <div class="user">
-                        <img src="images/profile-2.jpg">
-                        <h2>Jack</h2>
-                        <p>54 Min Ago</p>
-                    </div>
-                    <div class="user">
-                        <img src="images/profile-3.jpg">
-                        <h2>Amir</h2>
-                        <p>3 Hours Ago</p>
-                    </div>
-                    <div class="user">
-                        <img src="images/profile-4.jpg">
-                        <h2>Ember</h2>
-                        <p>6 Hours Ago</p>
-                    </div>
-                    <div class="user">
-                        <img src="images/plus.png">
-                        <h2>More</h2>
-                        <p>New User</p>
-                    </div>
+                    <?php
+                    include "../scripts/conexion.php";
+
+                    $sql = "SELECT * FROM usuario";
+                    $resultado = $conn->query($sql);
+
+                    if ($resultado->num_rows > 0) {
+                        while ($row = $resultado->fetch_assoc()) {
+                            echo '<div class="user">';
+                            echo '<img src="../uploads/'. $row['foto'] .'">';
+                            echo '<h2>'. $row['nombre'] .'</h2>';
+                            echo '<p>'. $row['mail'] .'</p>';
+
+                        }
+                    } else {
+                        echo '<p style="margin:auto;">No hay usuarios registrados</p>';
+                    }
+                    $conn->close();
+                    ?>
+                    
+                    
                 </div>
             </div>
             <!-- End of New Users Section -->
